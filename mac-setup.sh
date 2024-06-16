161c52999bca6aef9320c2624ec5d698c1cf0932
mayank-ramnani <mayankr99@gmail.com>
2024-06-16 13:47:20 -0400

    add mac setup

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,3 +1,16 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# add commands to add brew to path
+
 brew install fzf
 brew install vim
 brew install tmux
+
+# install iterm2
+
+# install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k for ohmyzsh
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
